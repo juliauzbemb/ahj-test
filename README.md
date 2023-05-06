2614f5fa2dad8037ac2a9d2cad3eaa8b3276816f
juliauzbemb <90088899+juliauzbemb@users.noreply.github.com>
2023-05-06 22:30:36 +0300

    test commit

---
+++ README.md
@@ -0,0 +1,13 @@
+что нужно
+1. git
+2. node / npm / yarn
+3. webpack / parcel / rollup / vite
+4. jest / vitest / mocha + chai
+5. linter = eslint + prettier
+6. ci/cd = appveyor / github actions
+
+
+
+!!! yarn set version stable
+
+https://standardjs.com/
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: './index.html'
+    })
+    ],
+};
